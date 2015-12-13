dff546cf777371bb8b81cb472700c90494368d44
Mark Shilton <mark.shilton@gmail.com>
2015-12-13 16:19:49 +0000

    Add ed second UKf to test harness to compare 1 and 2 dimensional filters

--- Test_harness.py
+++ Test_harness.py
@@ -1,6 +1,32 @@
 
 from Unscented_kalman_filter import UnscentedKalmanFilter
+import numpy as np
 
+#Initialisa the unscented kalman filter
 ukf = UnscentedKalmanFilter(state_dimensions=2, measurement_dimensions=1, rate_change=1, state_transistion_func="null", measurement_transistion_func="null")
 
+#Initialise the filter points sigma point variables (alpha/beta/kappa)
 ukf.initSigmaPoints(alpha=2,beta=1,kappa=1)
+
+#set up mean and covariance variables
+covariance = np.eye(2)
+covariance *= 1.2
+mean = np.array([1, 1])
+
+#Test out generating sigma points based on the mean and covariance variables
+ukf.sigmaPoints(mean, covariance)
+
+
+#Initialisa the unscented kalman filter
+ukf = UnscentedKalmanFilter(state_dimensions=1, measurement_dimensions=1, rate_change=1, state_transistion_func="null", measurement_transistion_func="null")
+
+#Initialise the filter points sigma point variables (alpha/beta/kappa)
+ukf.initSigmaPoints(alpha=2,beta=1,kappa=1)
+
+#set up mean and covariance variables
+covariance = np.eye(1)
+covariance *= 1.2
+mean = np.array([2])
+
+#Test out generating sigma points based on the mean and covariance variables
+ukf.sigmaPoints(mean, covariance)
--- Unscented_kalman_filter.py
+++ Unscented_kalman_filter.py
@@ -12,7 +12,8 @@ class UnscentedKalmanFilter(object):
     def __init__(self, state_dimensions, measurement_dimensions, rate_change, state_transistion_func, measurement_transistion_func):
 
         # Number of variables in the state i.e x-position, y-poistion, x velociy, etc
-        self.x_dim = state_dimensions
+        self.x_dim = np.zeros(state_dimensions)
+        self.x_size = state_dimensions
         # Number of variables in the measurement unction
         self.h_dim = measurement_dimensions
         # How often the filter is called..
@@ -22,7 +23,7 @@ class UnscentedKalmanFilter(object):
         self.hx = measurement_transistion_func
 
         # Measurement noise function
-        self.R = 0
+        self.R = np.eye(state_dimensions, dtype=float)
 
         #Process noise function
         self.Q = 0
@@ -30,25 +31,54 @@ class UnscentedKalmanFilter(object):
         self.lamda = 0
 
         #Output
-        print '{0}= {1}'.format('State Dimensions', self.x_dim)
-        print '{0}= {1}'.format('Measurement Dimensions', self.h_dim)
+        print '{0}= {1}'.format('State Dimensions', np.size(self.x_dim))
+        print '{0}= {1}'.format('Measurement Dimensions', np.size(self.h_dim))
         print '{0}= {1}'.format('Rate Change', self.dt)
 
     """Function generates appropiate sigma points based on input variables"""
     def initSigmaPoints(self, alpha, beta, kappa):
         #calculate the lamda value that will be used later on
-        self.lamda = ((alpha**2)*(self.x_dim + kappa)) - self.x_dim
+        self.lamda = ((alpha**2)*(self.x_size + kappa)) - self.x_size
 
         # Work out the number of sigma points based on the number of state Dimensions
         # i.e 1 dimension equals 3 sigma points. 2 dimensions equals 5 sigm points
-        self.sigmas = np.zeros((2*self.x_dim +1,self.x_dim))
+        self.sigmas = np.zeros((2*self.x_size +1,self.x_size))
 
-        print 'Number of Sigmas points is:'
-        print (self.sigmas)
+        #print 'Number of Sigmas points is:'
+        #print (self.sigmas)
         return
 
     """Calculates the weights for the sigma points"""
     def sigmaPoints(self, mean, cov):
+
+        #Check that the means is the same dimensionality as the initialised UKF
+        assert self.x_size == np.size(mean),  "expected size {}, but size is {}".format(
+            self.x_size, np.size(mean) )
+
+        # Make sure mean is a in the array format and convert if necessary
+        if np.isscalar(mean):
+            mean = np.asarray([mean])
+
+        # Make sure he covariance is a matrix
+        if np.isscalar(cov):
+            cov = np.eye(n)*cov
+
+        # Calculating the sigma points
+        # Already found lamda from 'initSigmaPoints' function
+        diff = np.sqrt((self.x_size + self.lamda)*cov)
+
+        #sigma matrix already initialised in 'initSigmaPoints' function
+        # The first sigma point is the mean
+        self.sigmas[0] = mean
+
+        for k in range(self.x_size):
+            self.sigmas[k+1] = np.add(mean, diff[k])
+            self.sigmas[self.x_size + k + 1] = np.subtract(mean, diff[k])
+
+        print ("Generated Sigma points")
+        print self.sigmas
+
+        # loop through
 
         return
 
